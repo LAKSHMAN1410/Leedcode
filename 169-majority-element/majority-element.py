f66e4d6987a0baa93b6bd6e5e8cead82db6c1c95
LAKSHMANAN.S <sivalakshmansivan@gmail.com>
2026-03-17 01:31:55 +0530

    Time: 23 ms (8.83%) | Memory: 13.4 MB (98.54%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d=Counter(nums)
+        for k,v in d.items():
+            if v>len(nums)/2:
+                return k
