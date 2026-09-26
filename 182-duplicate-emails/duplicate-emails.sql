4badc367a43bcfaf5722501abad3fef09af61cf5
LAKSHMANAN.S <sivalakshmansivan@gmail.com>
2026-09-27 00:21:17 +0530

    Time: 348 ms (97.55%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,4 +1,4 @@
--- Write your PostgreSQL query statement below
-SELECT email FROM PERSON
+# Write your MySQL query statement below
+SELECT email FROM Person
 GROUP BY email
 HAVING COUNT(email) > 1;
